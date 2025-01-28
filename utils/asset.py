fb1769812df26319ae3be5956d6f5a8519b28f23
amirhosseinh77 <amirhossein261077@live.com>
2025-01-28 14:35:18 +0100

    added RL

---
+++ utils/asset.py
@@ -0,0 +1,21 @@
+import torch
+
+def compute_rewards_to_go(rewards, gamma):
+    rewards_rav = rewards.ravel()
+    rewards_to_go = torch.zeros_like(rewards_rav, dtype=torch.float, device=rewards.device)
+    cumulative_reward = 0
+    for t in range(len(rewards_rav)-1, -1, -1):
+        cumulative_reward = rewards_rav[...,t] + gamma * cumulative_reward
+        rewards_to_go[t] = cumulative_reward
+    return rewards_to_go.reshape(rewards.shape)
+
+def map_to_range(action, range):
+    min_val, max_val = range
+    mapped_action = ((action + 1) / 2) * (max_val - min_val) + min_val
+    return mapped_action
+
+def np_to_torch(x):
+    return torch.tensor(x, dtype=torch.float32).unsqueeze(0)
+
+def torch_to_np(x):
+    return x.squeeze(0).cpu().detach().numpy().ravel()
---
+++ utils/buffer.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+from collections import deque
+
+# Define the memory buffer to store experience tuples
+class ReplayBuffer():
+    def __init__(self, buffer_size):
+        self.buffer = deque(maxlen=buffer_size)
+
+    def push(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        # Sample a batch of experiences
+        sampled_experiences = random.sample(self.buffer, batch_size)
+        # Transpose the list of experiences, then convert each component to a NumPy array
+        sampled_experiences = [np.array(x) for x in zip(*sampled_experiences)]
+        # Ensure each component has at least 2 dimensions
+        return [x if x.ndim >= 2 else np.expand_dims(x, axis=-1) for x in sampled_experiences]
+
+    def __len__(self):
+        return len(self.buffer)
